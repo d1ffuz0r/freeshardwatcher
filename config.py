cb5fb45e241b42757a57ba8d1413154406e398e2
d1ffuz0r <d1fffuz0r@gmail.com>
2011-12-25 18:52:56 +0300

    prepared for github

---
+++ config.py
@@ -0,0 +1,17 @@
+"""Config"""
+config = {
+    'db_prod': {
+        'host': '',
+        'database': '',
+        'user': '',
+        'password': ''
+    },
+    'db_test': {
+        'host': 'localhost',
+        'database': 'l2planet',
+        'user': 'root',
+        'password': 'root'
+    }
+}
+
+conf = config["db_test"]
